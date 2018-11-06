45f72d4e0132f9e9feab6615350c02229dffc278
rpuxa <gr0070@mail.ru>
2018-11-06 23:23:29 +0400

    Add spaces

--- app/src/main/java/com/yatochk/translator/model/ModelContract.kt
+++ app/src/main/java/com/yatochk/translator/model/ModelContract.kt
@@ -4,6 +4,7 @@ import com.yatochk.translator.model.database.DatabaseTranslate
 
 object ModelContract {
     interface Contract {
+        
         fun savedTranslate()
         fun translate(text: String, fromLang: String, toLang: String)
         fun languageList(uiLanguage: String)
@@ -11,6 +12,7 @@ object ModelContract {
     }
 
     interface OnModelTaskListener {
+        
         fun onGetSavedTranslate(arrayDatabaseTranslates: ArrayList<DatabaseTranslate>)
         fun onRemoveTranslate(rowId: String)
         fun onAddTranslate(translate: DatabaseTranslate)
